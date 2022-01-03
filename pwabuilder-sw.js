44900464c729a63d1d5c1b60cf3fcf5c60fc6fe3
Lim Chunwei <win98se@users.noreply.github.com>
2022-01-03 15:31:15 +0800

    PWA 细节修改 (第八次)
    再度尝试修复离线“未缓存此页”错误信息；尝试添加页面计数

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,31 +1,28 @@
-const CACHE="pwabuilder-offline-page";
+const CACHE='pwabuilder-offline-page';
 
 importScripts('https://cdn.jsdelivr.net/npm/workbox-sw');
 
-const offlineFallbackPage="/offline.html";
+const offlineFallbackPage='/offline.html';
 
-self.addEventListener("message", (event)=>{
-	if(event.data && event.data.type==="SKIP_WAITING") {
+self.addEventListener('message', (event)=>{
+	if((event.data)&&(event.data.type==='SKIP_WAITING')) {
 		self.skipWaiting();
 	}
 });
 
 self.addEventListener('install', async (event)=>{
-	event.waitUntil(
-		caches.open(CACHE).then((cache)=>cache.add(offlineFallbackPage))
-	);
+	event.waitUntil(caches.open(CACHE).then((cache)=>{
+		return cache.add(offlineFallbackPage);
+	}));
 });
 
 if(workbox.navigationPreload.isSupported()) {
 	workbox.navigationPreload.enable();
 }
 
-workbox.routing.registerRoute(
-	new RegExp('/*'),
-	new workbox.strategies.StaleWhileRevalidate({
-		cacheName: CACHE
-	})
-);
+workbox.routing.registerRoute(/\/*/, new workbox.strategies.StaleWhileRevalidate({
+	cacheName: CACHE
+}));
 
 self.addEventListener('fetch', (event)=>{
 	if(event.request.mode==='navigate') {
